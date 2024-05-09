c19146fb288b0c981d8011628a26a17a2c587947
ngoduc <ngoduc202@gmail.com>
2024-05-09 09:18:45 +0700

    responsive tablet fixing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,21 @@
-import { Flex, Image, Typography, Menu } from "antd";
+import { Flex, Image, Typography, Menu, Button } from "antd";
 import {
   AppstoreOutlined,
   MailOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
   SettingOutlined,
   UserOutlined,
 } from "@ant-design/icons";
 
 import style from "./header.module.css";
+import { useState } from "react";
 
 function Header() {
+  const [collapsed, setCollapsed] = useState(false);
+  const [hiddenState, setHidden] = useState(true);
   const { Title } = Typography;
-  const { title_h3_logo } = style;
+  const { title_h3_logo, btn_collapse, menu2_display, menu2_hidden } = style;
   const items = [
     {
       label: "Giới thiệu",
@@ -66,34 +71,58 @@ function Header() {
   const onClick = (e) => {
     console.log("click ", e);
   };
+
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+    setHidden(!hiddenState);
+  };
   return (
-    <Flex
-      vertical={false}
-      align="center"
-      justify="space-between"
-      className={`${style["header"]} px-4`}
-    >
-      <Flex align="center">
-        <Image src="/logo.svg"></Image>
-        <Title
-          className={title_h3_logo}
-          level={3}
-          style={{ color: "white", marginLeft: "10px", marginBottom: "0" }}
+    <div className="headerr">
+      <Flex
+        vertical={false}
+        align="center"
+        justify="space-between"
+        className={`${style["header"]} px-4`}
+      >
+        <Flex align="center">
+          <Image src="/logo.svg"></Image>
+          <Title
+            className={title_h3_logo}
+            level={3}
+            style={{ color: "white", marginLeft: "10px", marginBottom: "0" }}
+          >
+            Pro Display
+          </Title>
+        </Flex>
+        <Button
+          type="default"
+          onClick={toggleCollapsed}
+          className={btn_collapse}
         >
-          Pro Display
-        </Title>
+          {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+        </Button>
+        <Menu
+          onClick={onClick}
+          mode="horizontal"
+          items={items}
+          theme="dark"
+          className={style["menu_header"]}
+        />
+        <Menu
+          onClick={onClick}
+          defaultSelectedKeys={["1"]}
+          defaultOpenKeys={["sub1"]}
+          mode="inline"
+          theme="dark"
+          inlineCollapsed={hiddenState}
+          items={items}
+          className={hiddenState ? menu2_hidden : menu2_display}
+        />
+        <div className={style["user_logo"]}>
+          <UserOutlined />
+        </div>
       </Flex>
-      <Menu
-        onClick={onClick}
-        mode="horizontal"
-        items={items}
-        theme="dark"
-        className={style["menu_header"]}
-      />
-      <div className={style["user_logo"]}>
-        <UserOutlined />
-      </div>
-    </Flex>
+    </div>
   );
 }
 
